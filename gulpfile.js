ab99f814010832d7d2b61dfaae14a1ec2b11a61b
kobe24 <kobe24@qq.com>
2018-02-02 18:01:29 +0800

    配置gulpfile.js文件

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+var browserSync=require('browser-sync');
+
+gulp.task('comfile',function(){
+    gulp.src('./src/sass/*.scss')
+    .pipe(sass({outputstyle:'expanded'}).on('error',sass.logError))
+    .pipe(gulp.dest('./src/css/'))
+});
+gulp.task('jtsass',function(){
+    gulp.watch('./src/sass/*.scss',['comfile'])
+});
+
+gulp.task('bro',function(){
+    browserSync({
+        // server:'./src';
+        port:2020,
+        proxy:'http://localhost:9999',
+
+        files:['./src/**/*.html','./src/css/*.css','./src/api/*.php']
+    })
+    gulp.watch('./src/sass/*.scss',['comfile'])
+});
